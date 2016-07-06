cbf80eaae5edad655064edf2cd167b10899ead02
Rafal Wilinski <raf.wilinski@gmail.com>
2016-07-06 21:45:56 +0200

    Figured out drop-files event 🔥

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const menubar = require('menubar');
-const mb = menubar({
-  width: 400,
-  height: 300
-});
-
+const {app, Tray} = require('electron');
 require('electron-debug')({showDevTools: true});
 
-mb.on('ready', () => {
+app.on('ready', () => {
+  const tray = new Tray('./public/icon.png');
+  const mb = menubar({
+    width: 400,
+    height: 300,
+    tray
+  });
 
+  tray.on('drop-files', (event, files) => {
+    console.log(event);
+    console.log(files);
+  });
 });
-
-
